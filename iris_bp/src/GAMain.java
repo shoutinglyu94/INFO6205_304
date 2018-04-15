875ac3cb5cef66ce662d8645defcae9772a2717e
Shouting Lyu <lyu.s@husky.neu.edu>
2018-04-15 16:37:35 -0400

    lst

--- iris_bp/src/GAMain.java
+++ iris_bp/src/GAMain.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class GATEST {
+public class GAMain {
     public static void main(String args[]) throws Exception {
         GA gatest = new GA();
         gatest.caculte();
